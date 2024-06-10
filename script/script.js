8a689c9be1f5e680beb686e5c309e29a91ba7453
Trenclik <trenclikb1@gmail.com>
2024-06-10 21:31:32 +0200

    header
    nedokončený header

---
+++ script/script.js
@@ -0,0 +1,9 @@
+function zmenaPodleClass(x){
+    document.body.style.backgroundImage = "url('/style/pic/2.png')";
+    
+}
+function zmenaZpet(){
+    document.body.style.backgroundImage = "url('/style/pic/1.png')";
+
+}
+
