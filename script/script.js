93ecb73d493944ff8f03e242491f2124c017d8a9
pohrebacek <petrjan872@gmail.com>
2024-06-15 22:29:17 +0200

    Merge branch 'main' of https://github.com/mefedroniny/eternitystr-nka

---
+++ script/script.js
@@ -0,0 +1,10 @@
+function zmenaPodleClass(x){
+    document.body.style.backgroundImage = "url('/style/pic/2.png')";
+    
+}
+function zmenaZpet(){
+    document.body.style.backgroundImage = "url('/style/pic/1.png')";
+    
+
+}
+
